59aaeb64a0e98661001043425631e67ea1897e38
biswajitmee <biswajit.mee@gmail.com>
2025-04-26 12:12:10 +0530

    about horizontal done

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-vite.config.js
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
-// import fs from 'fs'
-// import path from 'path'
 
+// // https://vite.dev/config/
 // export default defineConfig({
-//   base: '/', // <--- VERY IMPORTANT
-//   plugins: [
-//     react(),
-//     {
-//       name: 'netlify-redirects',
-//       closeBundle() {
-//         const outDir = 'dist'
-//         const redirectsPath = path.join(outDir, '_redirects')
-//         const content = '/*    /index.html   200\n'
-
-//         fs.writeFileSync(redirectsPath, content)
-//         console.log('✅ Netlify _redirects file written')
-//       }
-//     }
-//   ]
+//   plugins: [react()],
 // })
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+
+export default defineConfig({
+  base: '/', // <--- VERY IMPORTANT
+  plugins: [
+    react(),
+    {
+      name: 'netlify-redirects',
+      closeBundle() {
+        const outDir = 'dist'
+        const redirectsPath = path.join(outDir, '_redirects')
+        const content = '/*    /index.html   200\n'
+
+        fs.writeFileSync(redirectsPath, content)
+        console.log('✅ Netlify _redirects file written')
+      }
+    }
+  ]
+})
